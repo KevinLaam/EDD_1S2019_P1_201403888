b2cad10a5e277ce047e1abe2e2f16533ec5746bd
Kevin <klaam113@gmail.com>
2019-08-12 00:36:52 -0600

    version3

--- Lista.py
+++ Lista.py
@@ -10,12 +10,12 @@ class listaSimple:
         self.primero = None
         self.ultimo = None
     
-    def estaVacia():
+    def estaVacia(self):
         return True if self.primero == None else False
 
     def agregarInicio(self, nombre,score):
         nuevoNodo = nodoLista(nombre,score)
-        if(self.estaVacia):
+        if(self.estaVacia()):
             self.primero = nuevoNodo
             self.ultimo = self.primero
             return
@@ -23,9 +23,9 @@ class listaSimple:
         self.primero.anterior = nuevoNodo
         self.primero = nuevoNodo
     
-    def agregarFinal(self,nombre,score)
+    def agregarFinal(self,nombre,score):
         nuevoNodo = nodoLista(nombre,score)
-        if(self.estaVacia())
+        if(self.estaVacia()):
             self.primero = nuevoNodo
             self.ultimo = self.primero
             return
@@ -53,8 +53,24 @@ class listaSimple:
         else:   
             self.primero = self.primero.siguiente
             self.primero.anterior = None
-            
+        
     def mostrar(self):
+        if(self.estaVacia()):
+            print("lista doble vacia")
+        else:
+            aux = self.primero
+            while(aux!= None):
+                print("usuario"+str(aux.nombre)+"Punteo"+str(aux.score))
+                aux = aux.siguiente
+                            
+            
+lista = listaSimple()
+lista.agregarInicio("oscar",57)
+lista.agregarInicio("oscar2",58)
+lista.agregarInicio("oscar3",59)
+lista.agregarInicio("oscar4",60)
+print("-----")
+lista.mostrar()
         
             
    
--- ListaCircular.py
+++ ListaCircular.py
@@ -6,17 +6,15 @@ class Nodo():
 
 class ListaCircular():
     def __init__(self):
-        primero = None
-        ultimo = None
+        self.primero = None
+        self.ultimo = None
     
-    def estaVacia():
-        return True if primero.siguiente == None else False
+    def estaVacia(self):
+        return True if self.primero == None else False
 
     def agregarInicio(self, usuario):
         nuevoNodo = Nodo(usuario)
         if(self.estaVacia()):
-            self.nuevoNodo.siguiente = nuevoNodo
-            self.nuevoNodo.anterior = nuevoNodo
             self.primero = self.ultimo = nuevoNodo
             return
         nuevoNodo.anterior = self.ultimo
@@ -24,8 +22,32 @@ class ListaCircular():
         self.ultimo.siguiente = nuevoNodo
         self.primero.anterior = nuevoNodo
         self.primero = nuevoNodo
+    
+    def agregarFinal(self,usuario):
+        nuevoNodo = Nodo(usuario)
+        if(self.estaVacia()):
+            nuevoNodo.siguiente = nuevoNodo
+            nuevoNodo.anterior = nuevoNodo
+            self.primero = self.ultimo = nuevoNodo
+            return
+        nuevoNodo.anterior = self.ultimo
+        nuevoNodo.siguiente = self.primero
+        self.ultimo.siguiente = nuevoNodo
+        self.primero.anterior = nuevoNodo
+        self.ultimo = nuevoNodo
+    
+    
+    
+    def mostrar(self):
+        if(self.estaVacia()):
+            print("lista circular doble vacia")
+        else:
+            aux = self.primero
+            while(aux.siguiente != self.primero):
+                print("usuario"+str(aux.usuario))
+                aux = aux.siguiente
 
 circular = ListaCircular()
 circular.agregarInicio("kevin")
 print("Se agrego el dato")
-
+circular.mostrar()
